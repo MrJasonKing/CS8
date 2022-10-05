e8d3ce2ecdc0ef163dda4cd258a0961023eedc77
Jason King <57264802+MrJasonKing@users.noreply.github.com>
2022-10-05 13:26:35 +0700

    Update Binary Search.py

--- Binary Search.py
+++ Binary Search.py
@@ -1,5 +1,4 @@
-# function takes a sorted array and an item. If the item is in the array,
-# the function returns its position (index). 
+# function takes a sorted array and an item. If the item is in the array, the function returns its position (index). 
 def binary_search(list, item):
   # low and high keep track of which part of the list you'll search in.
   low = 0
@@ -7,9 +6,7 @@ def binary_search(list, item):
 
   # While you haven't narrowed it down to one element 
   while low <= high:
-    # each time check the middle element
-    # // - floor division will divide the first argument by the second 
-    # and round the result down to the nearest whole number
+    # each time check the middle element/ // - floor division will divide the first argument by the second and round the result down to the nearest whole number
     mid = (low + high) // 2 
     guess = list[mid]
     # Found the item.
